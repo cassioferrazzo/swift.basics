4e2bc56bca9f2f868e84bff80ca2885715a5426a
Rayan Maluf <rayanmaluf@hotmail.com>
2016-09-19 15:36:37 -0300

    prata ok

---
+++ prata/prata/ImagemViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ImagemViewController.swift
+//  prata
+//
+//  Created by Student on 9/19/16.
+//  Copyright © 2016 Cassio Ferrazzo. All rights reserved.
+//
+
+import UIKit
+
+class ImagemViewController: UIViewController {
+    
+    @IBOutlet weak var twDescricao: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
